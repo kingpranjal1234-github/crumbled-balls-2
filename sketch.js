5981daad3886be9415ffb15cf9b7b9de6d33d3f9
kingpranjal1234-github <76901883+kingpranjal1234-github@users.noreply.github.com>
2021-02-09 21:38:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,71 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+
+var dustbinObject, paperObject,groundObject	, world;
+
+function preload(){
+
+dustbinImage=loadImage("dustbingreen.png")
+paperImage=loadImage("paper.png")
+
+
+	
+}
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	dustbinObject = new dustbin(1200,650);
+    dustbinObject = addImage("dustbinGreen.png");
+
+
+	paperObject = new paper(200,450,40);
+	paperObject = addImage("paperImage");
+
+	groundObject = new ground(width/2,670,width,20);
+	
+	
+
+	  var render = Render.create({
+	  element : document.body,
+	  engine : engine,
+	  options : {
+	    width : 1200,
+	    height: 700,
+	    wireframes: false
+	  }
+	});
+
+	Engine.run(engine);
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("lightblue");
+ 
+  
+  paperObject.display();
+  groundObject.display();
+  dustbinObject.display();
+ 
+  
+  
+ 
+}
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+    
+  	}
+}
